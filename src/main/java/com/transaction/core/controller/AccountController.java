eb8d1b8b28f7be24fd806c3362dab130683841e1
pritiranjanc <pritiranjan.c@gmail.com>
2026-09-25 20:18:58 +0530

    Refactored Junit

--- src/main/java/com/transaction/core/controller/AccountController.java
+++ src/main/java/com/transaction/core/controller/AccountController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,7 +21,8 @@ public class AccountController {
 
     @GetMapping("/{accountId}/transactions")
     public PageDTO<TransactionHistory> getTransactionHistory(
-            @PathVariable Long accountId, @RequestParam(required = false,name = "page",defaultValue = "0") Integer page,
+            @PathVariable Long accountId,
+            @RequestParam(required = false,name = "page",defaultValue = "0") Integer page,
             @RequestParam(required = false,name = "page-size", defaultValue = "10") Integer pageSize) {
         return accountService.getTransactionHistory(accountId,
                 PageRequest.of(page,pageSize, Sort.by(Sort.Direction.DESC, "createdAt")));
